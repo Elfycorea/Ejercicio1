cd0b66c4a99afd7960d15d2bee429119bef77d6e
ELFY-PC <ELFY-PC@LAPTOP-BL43SEFK>
2022-11-20 10:49:08 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Ejercicio1/MainPage.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+         
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+           
+        }
+
+        private void Pickerope_SelectIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private  void btnsuma_Clicked(object sender, EventArgs e)
+        {
+
+
+            int n1 = Convert.ToInt32(txtnumero1.Text);
+            int n2 = Convert.ToInt32(txtnumero2.Text);
+            int suma = n1 + n2;
+            DisplayAlert("Mensaje", suma.ToString(), "total");
+            
+
+
+        }
+
+        private void btnresta_Clicked(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(txtnumero1.Text);
+            int n2 = Convert.ToInt32(txtnumero2.Text);
+            int resta = n1 - n2;
+            DisplayAlert("Mensaje", resta.ToString(), "total");
+
+        }
+
+        private void btndivision_Clicked(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(txtnumero1.Text);
+            int n2 = Convert.ToInt32(txtnumero2.Text);
+            int division = n1 / n2;
+            DisplayAlert("Mensaje", division.ToString(), "total");
+        }
+
+        private void btnmultiplicacion_Clicked(object sender, EventArgs e)
+        {
+            int n1 = Convert.ToInt32(txtnumero1.Text);
+            int n2 = Convert.ToInt32(txtnumero2.Text);
+            int multiplicacion = n1 * n2;
+            DisplayAlert("Mensaje", multiplicacion.ToString(), "total");
+
+        }
+    }
+
+       
+}
---
+++ Ejercicio1/Ejercicio1/Models/FuncionesMatematicas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio1.Models
+{
+    public class FuncionesMatematicas
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+        public int suma { get; set; }
+        public int res { get; set; }
+        public int div { get; set; }
+        public int mult { get; set; }
+        public int result { get; set; }
+
+    }
+}
